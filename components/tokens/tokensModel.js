31f48c9c93cf747be9883fab2baa412847b8ff71
Quentin <qlaffont13@gmail.com>
2018-07-21 15:27:19 +0200

    Add Token Model

---
+++ components/tokens/tokensModel.js
@@ -0,0 +1,42 @@
+module.exports = mongoose => {
+    "use strict";
+
+    const Schema = mongoose.Schema;
+
+    /**
+     * @swagger
+     * definitions:
+     *   Token:
+     *     type: object
+     *     required:
+     *      - token
+     *      - id_User
+     *      - creation
+     *     properties:
+     *       label:
+     *         type: string
+     *       creation:
+     *         type: date
+     *         default: () => new Date().toISOString()
+     *       id_User:
+     *         $ref: '#/definitions/User'
+     */
+    const TokenSchema = Schema({
+        token: {
+            type: String,
+            required: true
+        },
+        creation: {
+            type: Date,
+            required: true,
+            default: () => new Date().toISOString()
+        },
+        id_User: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        }
+    });
+
+    return mongoose.model("Token", TokenSchema);
+};
